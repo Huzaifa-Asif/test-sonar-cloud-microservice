fce57b36687daa4cd600b55dc03887cb771a338c
Huzaifa Asif <huzaifa8580@gmail.com>
2023-10-11 15:50:03 +0800

    feat: create error

--- movie-review-app/app.js
+++ movie-review-app/app.js
@@ -50,6 +50,8 @@ app.use('/user', routeUser);
 app.use('/comment', routeComment);
 // Introducing a security vulnerability: No Rate Limiting
 app.post('/unprotected-login', (req, res) => {
+  
+  
   const { username, password } = req.body;
   // Simulate a login attempt without rate limiting
   if (username === "admin" && password === "password123") {
@@ -61,24 +63,24 @@ app.post('/unprotected-login', (req, res) => {
 
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+      // catch 404 and forward to error handler
+      app.use(function (req, res, next) {
+        next(createError(404));
+      });
 
 
 
-
+const A = "AAVVV";
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
 });
 
 
